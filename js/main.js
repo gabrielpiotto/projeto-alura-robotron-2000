4d6d0cabe7bdbdaf01072dd583ab90519a839424
Gabriel Piotto <gabrielpiottogdm@gmail.com>
2022-06-29 12:25:06 -0300

    Implementando calculo de atributos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
 // Variaveis {
 const controle = document.querySelectorAll("[data-controle]");
+
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 // }
 
 // Eventos {
@@ -7,6 +43,7 @@ const controle = document.querySelectorAll("[data-controle]");
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+        autalizaEstatisticas(evento.target.dataset.peca, evento.target.dataset.controle, evento.target.parentNode);
     });
 });
 
@@ -26,6 +63,21 @@ function manipulaDados(operacao, controle) {
 
     if (operacao === "+") {
         peca.value = parseInt(peca.value) + 1;
+    }
+}
+
+function autalizaEstatisticas(peca, operacao, controle) {
+    const contador = controle.querySelector("[data-contador]");
+
+    if (operacao === "-" && contador.value >= 1) {
+        estatisticas.forEach((elemento) => {
+            elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica]
+        })
+
+    } if (operacao === "+") {
+        estatisticas.forEach((elemento) => {
+            elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+        })
     }
 }
 
